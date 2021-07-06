90309a3750f68b7e4d8b11762e401955579c50cc
EliasMenezesJr <eliasmenezes2510@gmail.com>
2021-07-06 12:39:35 -0300

    aula 4 final

--- main.js
+++ main.js
@@ -18,10 +18,10 @@ for (const link of links) {
 }
 
 /* mudar o header da página quando der scroll */
-const header = document.querySelector("#header");
-const navHeight = header.offsetHeight;
+function changeHeaderWhenScroll() {
+  const header = document.querySelector("#header");
+  const navHeight = header.offsetHeight;
 
-window.addEventListener("scroll", function () {
   if (window.scrollY >= navHeight) {
     /* scroll é maior que a altura do header */
     header.classList.add("scroll");
@@ -29,32 +29,52 @@ window.addEventListener("scroll", function () {
     /* menor que a altura do header */
     header.classList.remove("scroll");
   }
-});
+}
 
 /* Testimonial carousel slider swiper */
-const swiper = new Swiper('.swiper-container', {
+const swiper = new Swiper(".swiper-container", {
   slidesPerView: 1,
   pagination: {
-    el: '.swiper-pagination'
+    el: ".swiper-pagination",
   },
   mousewheel: true,
-  Keyboard: true
-})
+  Keyboard: true,
+});
 
 /* ScrollReavel: Mostrar elementos quando der scroll na página */
 const scrollReavel = ScrollReveal({
-    origin: 'top',
-    distance: '30px',
-    duration: 700,
-    reset: true
-  })
+  origin: "top",
+  distance: "30px",
+  duration: 700,
+  reset: true,
+});
 
-  scrollReavel.reveal(`
+scrollReavel.reveal(
+  `
     #home .image , #home .text,
     #about .image, #about .text,
     #services header, #services .card,
     #testimonial header, #testimonials .testimonials
-    #contact .text, #contact .links
+    #contact .text, #contact .links,
+    footer .brand, footer .social
     `,
-    { interval: 100}
-  )
+  { interval: 100 }
+);
+
+/* Botão voltar para o topo */
+function backToTop() {
+  const backToTopButton = document.querySelector(".back-to-top");
+
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
+}
+
+/* When Scroll */
+window.addEventListener("scroll", function () {
+  changeHeaderWhenScroll();
+  backToTop()
+
+});
---
+++ notes.md
@@ -0,0 +1,17 @@
+/* aula 4 00:50:59 */
+
+
+## iconfinde
+https://www.iconfinder.com
+
+como usar:
+/* aula 2 00:40:00 */
+
+User: no figma selecione os icones que deseja usar, em seguida exporte em "Export (numero de icon selecionados) layers, localizado no canto direito inferior. Exporte ele em SVG!
+
+em seguida user
+## icomoon
+https://icomoon.io/app
+
+Lembre de ajustar o grid e retirar as cores.
+
