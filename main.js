e877c4cd32c6e26c76acaba725c4691943318f26
EliasMenezesJr <eliasmenezes2510@gmail.com>
2021-07-07 11:24:35 -0300

    aula 5

--- main.js
+++ main.js
@@ -39,6 +39,12 @@ const swiper = new Swiper(".swiper-container", {
   },
   mousewheel: true,
   Keyboard: true,
+  breakpoints:{
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true,
+    }
+  }
 });
 
 /* ScrollReavel: Mostrar elementos quando der scroll na página */
